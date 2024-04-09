b17560e623e6f4accdcdbb4c518028265e712c84
IcedComputer <31418197+IcedComputer@users.noreply.github.com>
2024-04-09 15:45:38 -0700

    .
    .

--- Updates/refresh.sh
+++ Updates/refresh.sh
@@ -20,8 +20,6 @@ function download()
 	#download an updated update.sh
 	curl --tlsv1.2 -o $TEMPDIR/updates.sh 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/Updates/updates.sh'
 	
-	## download an updated ListUpdater
-	curl --tlsv1.2 -o $TEMPDIR/ListUpdater.sh 'https://raw.githubusercontent.com/IcedComputer/Azure-Pihole-VPN-setup/master/ListUpdater.sh'
 	
 	## download an updated DB_updates.sh
 	curl --tlsv1.2 -o $TEMPDIR/DB_Updates.sh 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/Updates/DB_Updates.sh'
