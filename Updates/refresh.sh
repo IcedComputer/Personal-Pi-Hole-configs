d3a8cd6dfa80071f376c37e740676274778d98d8
IcedComputer <31418197+IcedComputer@users.noreply.github.com>
2024-08-13 14:43:21 -0700

    Update refresh.sh

--- Updates/refresh.sh
+++ Updates/refresh.sh
@@ -1,7 +1,7 @@
 ## Created 25 July 2020
-## Updated 21 Sep 2021
+## Updated 2024-08-13
 ## refresh.sh
-## This script simply updates the "updates.sh" script to ensure the local copy on the machine is up to date
+## This script simply updates the various update script to ensure the local copy on the machine is up to date
 ##
 
 
@@ -20,7 +20,6 @@ function download()
 	#download an updated update.sh
 	curl --tlsv1.2 -o $TEMPDIR/updates.sh 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/Updates/updates.sh'
 	
-	
 	## download an updated DB_updates.sh
 	curl --tlsv1.2 -o $TEMPDIR/DB_Updates.sh 'https://raw.githubusercontent.com/IcedComputer/Personal-Pi-Hole-configs/master/Updates/DB_Updates.sh'
 	
@@ -36,10 +35,7 @@ function move()
 	## change permisions
 	chmod 777 $TEMPDIR/updates.sh
 	mv $TEMPDIR/updates.sh $FINISHED/updates.sh
-	
-	chmod 777 $TEMPDIR/ListUpdater.sh
-	mv $TEMPDIR/ListUpdater.sh $FINISHED/ListUpdater.sh
-	
+		
 	chmod 777 $TEMPDIR/DB_Updates.sh
 	mv $TEMPDIR/DB_Updates.sh $FINISHED/DB_Updates.sh
 	
