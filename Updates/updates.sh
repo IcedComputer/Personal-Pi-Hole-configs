98e0a65a786be9daed691cf19ecafde0355449b7
IcedComputer <31418197+IcedComputer@users.noreply.github.com>
2024-08-14 10:44:10 -0700

    .

--- Updates/updates.sh
+++ Updates/updates.sh
@@ -1,4 +1,4 @@
-## Last Updated 20 April 2023
+## Last Updated 2024-08-14
 ## updates.sh
 ## This script is designed to keep the pihole updated and linked to any changes made
 ##
@@ -164,7 +164,7 @@ function encrypted_block_list()
 	gpg $TEMPDIR/encrypt.block.addlist.temp.gpg
 	wait
 	sed -i -e "s/\r//g" $TEMPDIR/encrypt.block.addlist.temp
-	mv $TEMPDIR/encrypt.block.addlist.temp $CONF/encrypt.list
+	mv $TEMPDIR/encrypt.block.addlist.temp /var/www/html/encrypt.list
 		
 }
 
