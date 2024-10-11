08c2112379dad8ef775d8b5f13cac693b5ce689c
IcedComputer <31418197+IcedComputer@users.noreply.github.com>
2024-10-11 11:36:59 -0700

    Update Research.sh

--- Updates/Research.sh
+++ Updates/Research.sh
@@ -8,7 +8,7 @@
 #!/bin/bash
 
 
-cat /var/log/pihole.log | grep -v blocked | grep -v CNAME | grep -v NODATA | grep -v HTTPS | grep -oP '(?<=reply\s)[^\s]+(?=\sis)' | sort | uniq -c | sort > /scripts/temp/passed.temp
-cat /var/log/pihole.log | grep blacklisted | grep -oP '[^\s]+(?=\sis)'| sort | uniq -c | sort > /scripts/temp/regex_filtered.temp
-cat /var/log/pihole.log | grep blocked | grep -oP '[^\s]+(?=\sis)' |sort | uniq -c | sort > /scripts/temp/blocked_filtered.temp
+cat /var/log/pihole.log | grep -v blocked | grep -v CNAME | grep -v NODATA | grep -v HTTPS | grep -oP '(?<=reply\s)[^\s]+(?=\sis)' | sort | uniq -c | sort -n > /scripts/temp/passed.temp
+cat /var/log/pihole.log | grep blacklisted | grep -oP '[^\s]+(?=\sis)'| sort | uniq -c | sort -n > /scripts/temp/regex_filtered.temp
+cat /var/log/pihole.log | grep blocked | grep -oP '[^\s]+(?=\sis)' | sort | uniq -c | sort -n > /scripts/temp/blocked_filtered.temp
 
